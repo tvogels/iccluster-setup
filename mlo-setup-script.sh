a8a56422000a93df997477fc8a66d62802985eef
Yoann Moulin <yoann.moulin@epfl.ch>
2018-01-23 15:08:47 +0100

    add --yes option to apt to avoid never ending waiting validation

--- mlo-setup-script.sh
+++ mlo-setup-script.sh
@@ -153,7 +153,7 @@ export LC_ALL=C
 
 ########################################
 ## Install Docker
-apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
